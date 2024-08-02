d4268d2f7640fb235b663e10c02534939c353b66
Nubila <104649124+DLIBYH@users.noreply.github.com>
2024-08-02 12:46:54 +0600

    Update plant.ino

--- plant.ino
+++ plant.ino
@@ -1,3 +1,6 @@
-#define BLYNK_PRINT Serial
-#include <SPI.h>
+#define BLYNK_TEMPLATE_ID "TMPLN0cZklzj"
+#define BLYNK_TEMPLATE_NAME "MoistureCorrect"
+#define BLYNK_AUTH_TOKEN "SLxAnuWfMuRyX6B4ySUgtcU_begMxBkT"
+#include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
+#define waterPump D4
